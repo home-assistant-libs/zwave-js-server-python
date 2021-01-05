1a6fb2dd229385c6eb13c345ee2ae5c2ad006246
Paulus Schoutsen <balloob@gmail.com>
2021-01-05 21:51:26 +0000

    Add node loading form state

---
+++ test/test_node.py
@@ -0,0 +1,51 @@
+"""Tests for controller model."""
+import json
+from zwave_js_server import node as node_pkg
+
+from . import load_fixture
+
+
+def test_from_state():
+    """Test from_state method."""
+    state = json.loads(load_fixture("basic_dump.txt").split("\n")[0])["state"]
+
+    node = node_pkg.Node.from_state(state["nodes"][0])
+
+    assert node.node_id == 1
+    assert node.index == 0
+    assert node.status == 4
+    assert node.ready is True
+    assert node.device_class == {
+      "basic": "Routing Slave",
+      "generic": "Static Controller",
+      "specific": "PC Controller",
+      "mandatorySupportedCCs": [],
+      "mandatoryControlCCs": ["Basic"]
+    }
+    assert node.is_listening is True
+    assert node.is_frequent_listening is False
+    assert node.is_routing is False
+    assert node.max_baud_rate == 40000
+    assert node.is_secure is False
+    assert node.version == 4
+    assert node.is_beaming is True
+    assert node.manufacturer_id == 134
+    assert node.product_id == 90
+    assert node.product_type == 257
+    assert node.device_config == {
+      "manufacturerId": 134,
+      "manufacturer": "AEON Labs",
+      "label": "ZW090",
+      "description": "Z‐Stick Gen5 USB Controller",
+      "devices": [
+        { "productType": "0x0001", "productId": "0x005a" },
+        { "productType": "0x0101", "productId": "0x005a" },
+        { "productType": "0x0201", "productId": "0x005a" }
+      ],
+      "firmwareVersion": { "min": "0.0", "max": "255.255" },
+      "associations": {},
+      "paramInformation": { "_map": {} }
+    }
+    assert node.label == "ZW090"
+    assert node.neighbors == [31, 32, 33, 36, 37, 39, 52]
+    assert node.interview_attempts == 1
---
+++ zwave_js_server/node.py
@@ -0,0 +1,76 @@
+"""Z-Wave node."""
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Node:
+    node_id: int
+    index: int
+    installer_icon: int
+    user_icon: int
+    status: int
+    ready: bool
+    device_class: str # XXXXXX
+    is_listening: bool
+    is_frequent_listening: bool
+    is_routing: bool
+    max_baud_rate: int
+    is_secure: bool
+    version: int
+    is_beaming: bool
+    manufacturer_id: int
+    product_id: int
+    product_type: int
+    firmware_version: str
+    zwave_plus_version: int
+    node_type: int
+    role_type: int
+    name: str
+    location: str
+    device_config: str # XXXXXX
+    label: str
+    neighbors: List[int]
+    endpoint_count_is_dynamic: bool
+    endpoints_have_identical_capabilities: bool
+    individual_endpoint_count: int
+    aggregated_endpoint_count: int
+    interview_attempts: int
+
+    @classmethod
+    def from_state(cls, data):
+        return cls(
+            node_id=data.get("nodeId"),
+            index=data.get("index"),
+            installer_icon=data.get("installerIcon"),
+            user_icon=data.get("userIcon"),
+            status=data.get("status"),
+            ready=data.get("ready"),
+            device_class=data.get("deviceClass"),
+            is_listening=data.get("isListening"),
+            is_frequent_listening=data.get("isFrequentListening"),
+            is_routing=data.get("isRouting"),
+            max_baud_rate=data.get("maxBaudRate"),
+            is_secure=data.get("isSecure"),
+            version=data.get("version"),
+            is_beaming=data.get("isBeaming"),
+            manufacturer_id=data.get("manufacturerId"),
+            product_id=data.get("productId"),
+            product_type=data.get("productType"),
+            firmware_version=data.get("firmwareVersion"),
+            zwave_plus_version=data.get("zwavePlusVersion"),
+            node_type=data.get("nodeType"),
+            role_type=data.get("roleType"),
+            name=data.get("name"),
+            location=data.get("location"),
+            device_config=data.get("deviceConfig"),
+            label=data.get("label"),
+            neighbors=data.get("neighbors"),
+            endpoint_count_is_dynamic=data.get("endpointCountIsDynamic"),
+            endpoints_have_identical_capabilities=data.get(
+                "endpointsHaveIdenticalCapabilities"
+            ),
+            individual_endpoint_count=data.get("individualEndpointCount"),
+            aggregated_endpoint_count=data.get("aggregatedEndpointCount"),
+            interview_attempts=data.get("interviewAttempts"),
+        )
