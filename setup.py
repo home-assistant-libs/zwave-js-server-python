8e1121e598a3c146b5daf372563769ff01d565a5
Paulus Schoutsen <balloob@gmail.com>
2021-01-07 10:15:28 +0000

    Version bump to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "2021.1.0"
+VERSION = "0.1"
 
 
 setup(
