aa640d4f11759335d42984e920eb583013c3a869
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-08 09:24:07 +0100

    fix value id and typo

--- zwave_js_server/model/value.py
+++ zwave_js_server/model/value.py
@@ -11,7 +11,7 @@ def value_id(node: "Node", event: dict) -> str:
     """Return ID of value."""
     command_class = event.get("commandClass")
     endpoint = event.get("endpoint")
-    property_key_name = event.get("propertyKeyName")
+    property_key_name = event.get("propertyKeyName") or event.get("property")
     return f"{node.node_id}-{command_class}-{endpoint}-{property_key_name}"
 
 
@@ -103,7 +103,7 @@ class Value:
         return self.data.get("endpoint")
 
     @property
-    def property_(self) -> str:
+    def property(self) -> str:
         """Return property."""
         return self.data.get("property")
 
