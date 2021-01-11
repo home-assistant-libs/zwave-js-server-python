04a9f3fa4b49b66443a46e69c0efc231086c9bab
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-11 08:35:01 +0100

    Fix default values in get_value_id lookup

--- zwave_js_server/model/value.py
+++ zwave_js_server/model/value.py
@@ -39,9 +39,9 @@ class ValueDataType(TypedDict, total=False):
 def get_value_id(node: "Node", event_data: ValueDataType) -> str:
     """Return ID of value."""
     command_class = event_data["commandClass"]
-    endpoint = event_data.get("endpoint", "00")
+    endpoint = event_data.get("endpoint") or "00"
     property_ = event_data["property"]
-    property_key_name = event_data.get("propertyKeyName", "00")
+    property_key_name = event_data.get("propertyKeyName") or "00"
     return f"{node.node_id}-{command_class}-{endpoint}-{property_}-{property_key_name}"
 
 
