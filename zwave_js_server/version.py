84dd0cb187f0fb5d5ab63cb9382063b9dc739264
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2021-01-25 10:46:03 +0100

    Apply suggestions from code review
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- zwave_js_server/version.py
+++ zwave_js_server/version.py
@@ -1,4 +1,6 @@
 """Version helper."""
+from typing import cast
+
 import aiohttp
 
 from .client import Client, VersionInfo
@@ -8,10 +10,10 @@ async def get_server_version(url: str, session: aiohttp.ClientSession) -> Versio
     """Return a server version."""
     client = Client(url, session, start_listening_on_connect=False)
 
-    async def handle_connected():
+    async def handle_connected() -> None:
         await client.disconnect()
 
     client.register_on_connect(handle_connected)
 
     await client.connect()
-    return client.version
+    return cast(VersionInfo, client.version)
