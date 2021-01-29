b79d67bc75c2f1a058c130ee1a5b70a5f11f6662
Martin Hjelmare <marhje52@gmail.com>
2021-01-29 19:42:41 +0100

    Use client session for get server version helper (#75)
    * Use session for server version helper
    
    * Add and fix test

--- zwave_js_server/version.py
+++ zwave_js_server/version.py
@@ -1,19 +1,13 @@
 """Version helper."""
-from typing import cast
-
 import aiohttp
 
-from .client import Client, VersionInfo
+from .model.version import VersionInfo
 
 
 async def get_server_version(url: str, session: aiohttp.ClientSession) -> VersionInfo:
     """Return a server version."""
-    client = Client(url, session, start_listening_on_connect=False)
-
-    async def handle_connected() -> None:
-        await client.disconnect()
-
-    client.register_on_connect(handle_connected)
-
-    await client.connect()
-    return cast(VersionInfo, client.version)
+    client = await session.ws_connect(url)
+    try:
+        return VersionInfo.from_message(await client.receive_json())
+    finally:
+        await client.close()
