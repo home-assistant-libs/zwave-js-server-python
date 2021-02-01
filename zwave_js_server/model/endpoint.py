c4bb1662e21763cd7200cef75b22d88b8f179ecf
raman325 <7243222+raman325@users.noreply.github.com>
2021-02-01 03:13:23 -0500

    re-add node_id

--- zwave_js_server/model/endpoint.py
+++ zwave_js_server/model/endpoint.py
@@ -24,6 +24,11 @@ class Endpoint:
         self.data = data
 
     @property
+    def node_id(self) -> int:
+        """Return node ID property."""
+        return self.data["nodeId"]
+
+    @property
     def index(self) -> int:
         """Return index property."""
         return self.data["index"]
