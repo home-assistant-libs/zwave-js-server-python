2f26f11fdbf111ff561a40f404b6928d5b1cc399
Martin Hjelmare <marhje52@gmail.com>
2021-02-01 10:20:17 +0100

    Bump version to 0.15.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "0.14.2"
+VERSION = "0.15.0"
 
 
 setup(
