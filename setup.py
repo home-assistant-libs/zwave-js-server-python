83d14a550b1bd648e9e2e8cd45f907cdd9a156a5
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2021-02-04 16:44:43 +0100

    Bump awesome version in setup.py (#99)

--- setup.py
+++ setup.py
@@ -20,7 +20,11 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["test.*", "test"]),
     python_requires=">=3.8",
-    install_requires=["aiohttp>3", "awesomeversion"],
+    install_requires=[
+        "aiohttp>3",
+        # We rely on a feature introduced in this version
+        "awesomeversion>=21.2.1",
+    ],
     entry_points={
         "console_scripts": ["zwave-js-server-python = zwave_js_server.__main__:main"]
     },
