d830da7879a2cd0ec992e13cbf169ece74c3c90e
raman325 <7243222+raman325@users.noreply.github.com>
2021-02-15 13:29:03 -0500

    use fstring concatenation

--- zwave_js_server/helpers.py
+++ zwave_js_server/helpers.py
@@ -1,10 +1,10 @@
 """Helper functions for zwave-js-server."""
 
 
-# https://stackoverflow.com/a/19053800
+# Adapted from https://stackoverflow.com/a/19053800
 def snake_to_camel_case(snake_str: str) -> str:
     """Convert snake string to camel case."""
     components = snake_str.split("_")
     # We capitalize the first letter of each component except the first one
     # with the 'title' method and join them together.
-    return components[0] + "".join(x.title() for x in components[1:])
+    return f"{components[0]}{''.join(x.title() for x in components[1:])}"
