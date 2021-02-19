a183df9853082e1d7267b128727cb83f3bc0b859
raman325 <7243222+raman325@users.noreply.github.com>
2021-02-18 19:42:55 -0500

    fix docstring

--- zwave_js_server/const.py
+++ zwave_js_server/const.py
@@ -228,7 +228,11 @@ class ThermostatOperatingState(IntEnum):
 
 
 class ThermostatSetpointType(Enum):
-    """Enum with all (known/used) Z-Wave Thermostat Setpoint Types."""
+    """
+    Enum with all (known/used) Z-Wave Thermostat Setpoint Types.
+    
+    Returns tuple of (property_key, property_key_name).
+    """
 
     # https://github.com/zwave-js/node-zwave-js/blob/master/packages/zwave-js/src/lib/commandclass/ThermostatSetpointCC.ts#L53-L66
     NA = (0, "N/A")
