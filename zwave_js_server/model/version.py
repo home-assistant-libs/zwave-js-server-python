bb4c4688dddd44e1a24f489746a61377632280f0
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-02-25 11:46:32 +0100

    update comment

--- zwave_js_server/model/version.py
+++ zwave_js_server/model/version.py
@@ -20,6 +20,10 @@ class VersionInfo:
             driver_version=msg["driverVersion"],
             server_version=msg["serverVersion"],
             home_id=msg["homeId"],
+            # scheme versions are sent in the response from scheme version 1+
+            # this info not present means the server is at scheme version 0
+            # at some point in time (when to stop  supporting scheme version 0),
+            # we could adjust this code and assume the keys are there.
             min_scheme_version=msg.get("minSchemeVersion", 0),
             max_scheme_version=msg.get("maxSchemeVersion", 0)
         )
