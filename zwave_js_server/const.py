e2d3fb33b1415c48df89b2f6e4e4508f2f8d6981
Martin Hjelmare <marhje52@gmail.com>
2021-03-18 16:51:16 +0100

    Handle server schema version 2 (#165)

--- zwave_js_server/const.py
+++ zwave_js_server/const.py
@@ -6,7 +6,7 @@ from typing import Dict, List
 # minimal server schema version we can handle
 MIN_SERVER_SCHEMA_VERSION = 1
 # max server schema version we can handle (and our code is compatible with)
-MAX_SERVER_SCHEMA_VERSION = 1
+MAX_SERVER_SCHEMA_VERSION = 2
 
 VALUE_UNKNOWN = "unknown"
 
