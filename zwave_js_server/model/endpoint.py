31ced3c919d617237daf45b8ae778294729211bd
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-03-20 13:23:52 -0400

    Add device class property to endpoint model (#172)
    * Add device class property to endpoint model
    
    * add tests
    
    * note device class is reequied

--- zwave_js_server/model/endpoint.py
+++ zwave_js_server/model/endpoint.py
@@ -6,12 +6,15 @@ https://zwave-js.github.io/node-zwave-js/#/api/endpoint?id=endpoint-properties
 
 from typing import Optional, TypedDict
 
+from .device_class import DeviceClass, DeviceClassDataType
+
 
 class EndpointDataType(TypedDict, total=False):
     """Represent an endpoint data dict type."""
 
     nodeId: int  # required
     index: int  # required
+    deviceClass: DeviceClassDataType  # required
     installerIcon: int
     userIcon: int
 
@@ -32,6 +35,11 @@ class Endpoint:
     def index(self) -> int:
         """Return index property."""
         return self.data["index"]
+
+    @property
+    def device_class(self) -> DeviceClass:
+        """Return the device_class."""
+        return DeviceClass(self.data["deviceClass"])
 
     @property
     def installer_icon(self) -> Optional[int]:
