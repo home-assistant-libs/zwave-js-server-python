b1c5a246693ef7df59b6e91375cd08c5b60d6daf
raman325 <7243222+raman325@users.noreply.github.com>
2021-04-08 13:39:51 -0400

    lint

--- zwave_js_server/util/node.py
+++ zwave_js_server/util/node.py
@@ -262,7 +262,8 @@ def _get_int_from_partials_dict(
 
         provided_partial_values.append(zwave_value)
         partial_value = _validate_and_transform_new_value(zwave_value, partial_value)
-        int_value += partial_value << partial_param_bit_shift(zwave_value.property_key)  # type: ignore
+        bit_shift = partial_param_bit_shift(zwave_value.property_key)  # type: ignore
+        int_value += partial_value << bit_shift
 
     # To set partial parameters in bulk, we also have to include cached values for
     # property keys that haven't been specified
