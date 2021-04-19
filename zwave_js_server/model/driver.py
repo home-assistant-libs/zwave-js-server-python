62127e4742f97694a8d7fb879e5a19c3c8a7cd16
raman325 <7243222+raman325@users.noreply.github.com>
2021-04-19 11:02:42 -0400

    make async_send_command private for node and driver

--- zwave_js_server/model/driver.py
+++ zwave_js_server/model/driver.py
@@ -47,10 +47,10 @@ class Driver(EventBase):
     def handle_all_nodes_ready(self, event: Event) -> None:
         """Process a driver all nodes ready event."""
 
-    async def async_send_command(
+    async def _async_send_command(
         self, command: str, require_schema: int = None, **kwargs: Any
     ) -> dict:
-        """Send a driver command."""
+        """Send a driver command. For internal use only."""
         return await self.client.async_send_command(
             {
                 "command": f"driver.{command}",
@@ -61,19 +61,19 @@ class Driver(EventBase):
 
     async def async_update_log_config(self, log_config: LogConfig) -> None:
         """Update log config for driver."""
-        await self.async_send_command(
+        await self._async_send_command(
             "update_log_config", config=log_config.to_dict(), require_schema=4
         )
 
     async def async_get_log_config(self) -> LogConfig:
         """Return current log config for driver."""
-        result = await self.async_send_command("get_log_config", require_schema=4)
+        result = await self._async_send_command("get_log_config", require_schema=4)
         return LogConfig.from_dict(result["config"])
 
     async def async_start_listening_logs(self) -> None:
         """Send command to start listening to log events."""
-        await self.async_send_command("start_listening_logs", require_schema=4)
+        await self._async_send_command("start_listening_logs", require_schema=4)
 
     async def async_stop_listening_logs(self) -> None:
         """Send command to stop listening to log events."""
-        await self.async_send_command("stop_listening_logs", require_schema=4)
+        await self._async_send_command("stop_listening_logs", require_schema=4)
