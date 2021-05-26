44dc13201715f197ecf6bc1916df7609ce2a7e1b
raman325 <7243222+raman325@users.noreply.github.com>
2021-05-26 00:09:50 -0400

    tweak approach, fix bugs, add tests

--- zwave_js_server/util/multicast.py
+++ zwave_js_server/util/multicast.py
@@ -1,6 +1,7 @@
 """Support for multicast commands."""
 
 from typing import Any, List, Optional, Union, cast
+from zwave_js_server.model.node import Node
 
 from ..client import Client
 from ..const import CommandClass
@@ -10,13 +11,13 @@ from ..model.value import Value, ValueDataType
 async def _async_send_command(
     client: Client,
     command: str,
-    node_ids: Optional[List[int]] = None,
+    nodes: Optional[List[Node]] = None,
     require_schema: Optional[int] = None,
     **kwargs: Any,
 ) -> dict:
     """Send a multicast command."""
-    if node_ids:
-        cmd = {"command": f"multicast_group.{command}", "node_ids": node_ids, **kwargs}
+    if nodes:
+        cmd = {"command": f"multicast_group.{command}", "nodeIDs": [node.node_id for node in nodes], **kwargs}
     else:
         cmd = {"command": f"broadcast_node.{command}", **kwargs}
 
@@ -27,7 +28,7 @@ async def async_multicast_set_value(
     client: Client,
     new_value: Any,
     val: Union[Value, ValueDataType],
-    node_ids: Optional[List[int]] = None,
+    nodes: Optional[List[Node]] = None,
 ) -> bool:
     """Send a multicast set_value command."""
     value_id = val.data if isinstance(val, Value) else val
@@ -35,7 +36,7 @@ async def async_multicast_set_value(
     result = await _async_send_command(
         client,
         "set_value",
-        node_ids,
+        nodes,
         valueId=value_id,
         value=new_value,
         require_schema=5,
@@ -44,11 +45,11 @@ async def async_multicast_set_value(
 
 
 async def async_multicast_get_endpoint_count(
-    client: Client, node_ids: Optional[List[int]] = None
+    client: Client, nodes: Optional[List[Node]] = None
 ) -> int:
     """Send a multicast get_endpoint_count command."""
     result = await _async_send_command(
-        client, "get_endpoint_count", node_ids, require_schema=5
+        client, "get_endpoint_count", nodes, require_schema=5
     )
     return cast(int, result["count"])
 
@@ -57,13 +58,13 @@ async def async_multicast_endpoint_supports_cc(
     client: Client,
     endpoint: int,
     command_class: CommandClass,
-    node_ids: Optional[List[int]] = None,
+    nodes: Optional[List[Node]] = None,
 ) -> bool:
     """Send a supports_cc command to a multicast endpoint."""
     result = await _async_send_command(
         client,
         "supports_cc",
-        node_ids,
+        nodes,
         index=endpoint,
         commandClass=command_class,
         require_schema=5,
@@ -75,13 +76,13 @@ async def async_multicast_endpoint_get_cc_version(
     client: Client,
     endpoint: int,
     command_class: CommandClass,
-    node_ids: Optional[List[int]] = None,
+    nodes: Optional[List[Node]] = None,
 ) -> int:
     """Send a get_cc_version command to a multicast endpoint."""
     result = await _async_send_command(
         client,
         "get_cc_version",
-        node_ids,
+        nodes,
         index=endpoint,
         commandClass=command_class,
         require_schema=5,
