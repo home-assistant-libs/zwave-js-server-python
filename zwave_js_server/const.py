1821e6dd78acfbc2ac0e43f7e044f2f3b555b405
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-07-12 22:09:12 -0400

    Add enum for Sound Switch CC toneId known values (#250)

--- zwave_js_server/const.py
+++ zwave_js_server/const.py
@@ -403,3 +403,11 @@ class ColorComponent(IntEnum):
     CYAN = 6
     PURPLE = 7
     INDEX = 8
+
+
+class ToneID(IntEnum):
+    """Enum with all known Sound Switch CC tone IDs."""
+
+    # https://github.com/zwave-js/node-zwave-js/blob/master/packages/zwave-js/src/lib/commandclass/SoundSwitchCC.ts#L71
+    OFF = 0
+    DEFAULT = 255
