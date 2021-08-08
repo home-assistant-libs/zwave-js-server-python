d1176bc653b97405bbe28e3eaf31622e5ecd5ef2
raman325 <7243222+raman325@users.noreply.github.com>
2021-08-08 16:55:41 -0400

    Remove guards, we will see what happens

--- zwave_js_server/util/command_class.py
+++ zwave_js_server/util/command_class.py
@@ -15,35 +15,22 @@ from ..exceptions import InvalidCommandClass
 from ..model.value import Value
 
 
-def get_meter_type(value: Value) -> Union[MeterType, None]:
+def get_meter_type(value: Value) -> MeterType:
     """Get the MeterType for a given value."""
     if value.command_class != CommandClass.METER:
         raise InvalidCommandClass(value, CommandClass.METER)
-
-    try:
-        return MeterType(value.metadata.cc_specific[CC_SPECIFIC_METER_TYPE])
-    except ValueError:
-        return None
+    return MeterType(value.metadata.cc_specific[CC_SPECIFIC_METER_TYPE])
 
 
-def get_meter_scale_type(value: Value) -> Union[MeterScaleType, None]:
+def get_meter_scale_type(value: Value) -> MeterScaleType:
     """Get the ScaleType for a given value."""
     meter_type = get_meter_type(value)
-    if meter_type is None:
-        return None
     scale_enum = METER_TYPE_TO_SCALE_ENUM_MAP[meter_type]
-    try:
-        return scale_enum(value.metadata.cc_specific[CC_SPECIFIC_SCALE])  # type: ignore
-    except ValueError:
-        return None
+    return scale_enum(value.metadata.cc_specific[CC_SPECIFIC_SCALE])  # type: ignore
 
 
-def get_multilevel_sensor_type(value: Value) -> Union[MultilevelSensorType, None]:
+def get_multilevel_sensor_type(value: Value) -> MultilevelSensorType:
     """Get the MultilevelSensorType for a given value."""
     if value.command_class != CommandClass.SENSOR_MULTILEVEL:
         raise InvalidCommandClass(value, CommandClass.SENSOR_MULTILEVEL)
-
-    try:
-        return MultilevelSensorType(value.metadata.cc_specific[CC_SPECIFIC_SENSOR_TYPE])
-    except ValueError:
-        return None
+    return MultilevelSensorType(value.metadata.cc_specific[CC_SPECIFIC_SENSOR_TYPE])
