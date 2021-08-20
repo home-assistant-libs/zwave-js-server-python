ebe9482dc9569f76c3318eef4595c3a8313bccba
raman325 <7243222+raman325@users.noreply.github.com>
2021-08-20 02:29:02 -0400

    Bump lib to 0.29.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "0.28.0"
+VERSION = "0.29.0"
 
 
 setup(
