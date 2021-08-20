829df60c7526fa733dda4f060ee9441df4130696
raman325 <7243222+raman325@users.noreply.github.com>
2021-08-20 01:55:55 -0400

    Add artificial tests to test when ccSpecific meterType or scale are invalid values

--- test/util/test_command_class.py
+++ test/util/test_command_class.py
@@ -2,6 +2,8 @@
 import pytest
 
 from zwave_js_server.const import (
+    CC_SPECIFIC_METER_TYPE,
+    CC_SPECIFIC_SCALE,
     CommandClass,
     ElectricScale,
     MeterType,
@@ -9,7 +11,7 @@ from zwave_js_server.const import (
 )
 from zwave_js_server.exceptions import InvalidCommandClass, UnknownValueData
 from zwave_js_server.model.node import Node
-from zwave_js_server.model.value import get_value_id
+from zwave_js_server.model.value import MetaDataType, get_value_id, Value, ValueDataType
 from zwave_js_server.util.command_class import (
     get_meter_scale_type,
     get_meter_type,
@@ -29,6 +31,25 @@ async def test_get_meter_type(inovelli_switch: Node):
     assert get_meter_type(node.values.get(value_id)) == MeterType.ELECTRIC
 
 
+async def test_get_invalid_meter_type(invalid_multilevel_sensor_type: Node):
+    """Test receiving an invalid meter type."""
+    node = invalid_multilevel_sensor_type
+
+    # Create value with an invalid meterType ID
+    metadata = MetaDataType(ccSpecific={CC_SPECIFIC_METER_TYPE: -1})
+    value_data = ValueDataType(
+        commandClass=CommandClass.METER,
+        commandClassName="Meter",
+        endpoint=0,
+        property="value",
+        propertyKey=9999999,
+        metadata=metadata,
+    )
+    value = Value(node, value_data)
+    with pytest.raises(UnknownValueData):
+        get_meter_type(value)
+
+
 async def test_get_meter_scale_type(inovelli_switch: Node):
     """Test get_meter_scale_type function."""
     node = inovelli_switch
@@ -37,6 +58,27 @@ async def test_get_meter_scale_type(inovelli_switch: Node):
     assert (
         get_meter_scale_type(node.values.get(value_id)) == ElectricScale.KILOWATT_HOUR
     )
+
+
+async def test_get_invalid_meter_scale_type(invalid_multilevel_sensor_type: Node):
+    """Test receiving an invalid meter scale type."""
+    node = invalid_multilevel_sensor_type
+
+    # Create value with an invalid scale ID
+    metadata = MetaDataType(
+        ccSpecific={CC_SPECIFIC_METER_TYPE: 1, CC_SPECIFIC_SCALE: -1}
+    )
+    value_data = ValueDataType(
+        commandClass=CommandClass.METER,
+        commandClassName="Meter",
+        endpoint=0,
+        property="value",
+        propertyKey=9999999,
+        metadata=metadata,
+    )
+    value = Value(node, value_data)
+    with pytest.raises(UnknownValueData):
+        get_meter_scale_type(value)
 
 
 async def test_get_multilevel_sensor_type(multisensor_6: Node):
