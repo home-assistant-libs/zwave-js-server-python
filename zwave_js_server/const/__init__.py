d859ef1174fd116978003414e70257e901e25b34
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-09-22 00:31:03 -0400

    Bump min server version and lib version (#298)
    * Bump min server version and lib version
    
    * Bump minor instead of patch version

--- zwave_js_server/const/__init__.py
+++ zwave_js_server/const/__init__.py
@@ -2,9 +2,9 @@
 from enum import Enum, IntEnum
 
 # minimal server schema version we can handle
-MIN_SERVER_SCHEMA_VERSION = 8
+MIN_SERVER_SCHEMA_VERSION = 9
 # max server schema version we can handle (and our code is compatible with)
-MAX_SERVER_SCHEMA_VERSION = 8
+MAX_SERVER_SCHEMA_VERSION = 9
 
 VALUE_UNKNOWN = "unknown"
 
