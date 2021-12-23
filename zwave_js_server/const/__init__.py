dce7d4494ab580fa5d7e907cc49a6308255694da
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-12-23 02:21:48 -0500

    Bump schema so we can use get_state commands

--- zwave_js_server/const/__init__.py
+++ zwave_js_server/const/__init__.py
@@ -2,9 +2,9 @@
 from enum import Enum, IntEnum
 
 # minimal server schema version we can handle
-MIN_SERVER_SCHEMA_VERSION = 12
+MIN_SERVER_SCHEMA_VERSION = 14
 # max server schema version we can handle (and our code is compatible with)
-MAX_SERVER_SCHEMA_VERSION = 13
+MAX_SERVER_SCHEMA_VERSION = 14
 
 VALUE_UNKNOWN = "unknown"
 
