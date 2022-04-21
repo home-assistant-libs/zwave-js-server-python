e98939d802d409c90aa17cd6e22f4769ee95ade5
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-04-21 00:32:41 -0400

    black

--- zwave_js_server/model/node/__init__.py
+++ zwave_js_server/model/node/__init__.py
@@ -792,7 +792,9 @@ class Node(EventBase):
                 self, cast(PowerLevelNotificationDataType, event.data)
             )
         else:
-            _LOGGER.warning("Unhandled notification command class: %s", command_class.name)
+            _LOGGER.warning(
+                "Unhandled notification command class: %s", command_class.name
+            )
 
     def handle_firmware_update_progress(self, event: Event) -> None:
         """Process a node firmware update progress event."""
