e47aa7f797d04309dadabbd9e4a9f3d155fefa7f
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-06-08 12:12:14 -0400

    Remove firmware changes so this is just about test cleanup

--- test/test_firmware.py
+++ test/test_firmware.py
@@ -1,106 +1,59 @@
 """Test the firmware update helper."""
-import pytest
+from unittest.mock import patch
 
-from zwave_js_server.exceptions import FailedCommand, FailedZWaveCommand, InvalidMessage
 from zwave_js_server.firmware import begin_firmware_update
 
-from .common import update_ws_client_msg_queue
-from .const import (
-    FAILED_COMMAND_MSG,
-    FAILED_ZWAVE_COMMAND_MSG,
-    INVALID_MESSAGE_MSG,
-    SUCCESS_MSG,
-)
 
-
-async def test_begin_firmware_update_guess_format(
-    client_session,
-    multisensor_6,
-    url,
-    version_data,
-    set_api_schema_data,
-    ws_client,
-):
+async def test_begin_firmware_update_guess_format(url, client_session, multisensor_6):
     """Test begin_firmware_update with guessed format."""
-    update_ws_client_msg_queue(
-        ws_client, (version_data, set_api_schema_data, SUCCESS_MSG)
-    )
+    with patch("zwave_js_server.firmware.Client.connect") as connect_mock, patch(
+        "zwave_js_server.firmware.Client.set_api_schema"
+    ) as set_api_schema_mock, patch(
+        "zwave_js_server.firmware.Client.async_send_command"
+    ) as cmd_mock, patch(
+        "zwave_js_server.firmware.Client.disconnect"
+    ) as disconnect_mock:
+        node = multisensor_6
+        await begin_firmware_update(url, node, "test", bytes(10), client_session)
 
-    await begin_firmware_update(url, multisensor_6, "test", bytes(10), client_session)
-
-    assert ws_client.receive_json.call_count == 3
-    assert ws_client.send_json.call_count == 2
-    command = ws_client.send_json.call_args[0][0]
-    command.pop("messageId")
-    assert command == {
-        "command": "node.begin_firmware_update",
-        "nodeId": multisensor_6.node_id,
-        "firmwareFilename": "test",
-        "firmwareFile": "AAAAAAAAAAAAAA==",
-    }
-    assert ws_client.close.call_count == 1
-
-
-async def test_begin_firmware_update_known_format(
-    client_session,
-    multisensor_6,
-    url,
-    version_data,
-    set_api_schema_data,
-    ws_client,
-):
-    """Test begin_firmware_update with known format."""
-    update_ws_client_msg_queue(
-        ws_client, (version_data, set_api_schema_data, SUCCESS_MSG)
-    )
-
-    await begin_firmware_update(
-        url,
-        multisensor_6,
-        "test",
-        bytes(10),
-        client_session,
-        "format",
-    )
-
-    assert ws_client.receive_json.call_count == 3
-    assert ws_client.send_json.call_count == 2
-    command = ws_client.send_json.call_args[0][0]
-    command.pop("messageId")
-    assert command == {
-        "command": "node.begin_firmware_update",
-        "nodeId": multisensor_6.node_id,
-        "firmwareFilename": "test",
-        "firmwareFile": "AAAAAAAAAAAAAA==",
-        "firmwareFileFormat": "format",
-    }
-    assert ws_client.close.call_count == 1
-
-
-@pytest.mark.parametrize(
-    "error_message, exception",
-    [
-        (INVALID_MESSAGE_MSG, InvalidMessage),
-        (FAILED_COMMAND_MSG, FailedCommand),
-        (FAILED_ZWAVE_COMMAND_MSG, FailedZWaveCommand),
-    ],
-)
-async def test_begin_firmware_update_failures(
-    client_session,
-    multisensor_6,
-    url,
-    version_data,
-    set_api_schema_data,
-    ws_client,
-    error_message,
-    exception,
-):
-    """Test begin_firmware_update failures."""
-    update_ws_client_msg_queue(
-        ws_client, (version_data, set_api_schema_data, error_message)
-    )
-
-    with pytest.raises(exception):
-        await begin_firmware_update(
-            url, multisensor_6, "test", bytes(10), client_session, "format"
+        connect_mock.assert_called_once()
+        set_api_schema_mock.assert_called_once()
+        cmd_mock.assert_called_once_with(
+            {
+                "command": "node.begin_firmware_update",
+                "nodeId": node.node_id,
+                "firmwareFilename": "test",
+                "firmwareFile": "AAAAAAAAAAAAAA==",
+            },
+            require_schema=5,
         )
+        disconnect_mock.assert_called_once()
+
+
+async def test_begin_firmware_update_known_format(url, client_session, multisensor_6):
+    """Test begin_firmware_update with known format."""
+    with patch("zwave_js_server.firmware.Client.connect") as connect_mock, patch(
+        "zwave_js_server.firmware.Client.set_api_schema"
+    ) as set_api_schema_mock, patch(
+        "zwave_js_server.firmware.Client.async_send_command"
+    ) as cmd_mock, patch(
+        "zwave_js_server.firmware.Client.disconnect"
+    ) as disconnect_mock:
+        node = multisensor_6
+        await begin_firmware_update(
+            url, node, "test", bytes(10), client_session, "test"
+        )
+
+        connect_mock.assert_called_once()
+        set_api_schema_mock.assert_called_once()
+        cmd_mock.assert_called_once_with(
+            {
+                "command": "node.begin_firmware_update",
+                "nodeId": node.node_id,
+                "firmwareFilename": "test",
+                "firmwareFile": "AAAAAAAAAAAAAA==",
+                "firmwareFileFormat": "test",
+            },
+            require_schema=5,
+        )
+        disconnect_mock.assert_called_once()
