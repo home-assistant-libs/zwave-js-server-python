06ab15caf8220ce71c89fe28e2edfe1df65a73aa
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-09-17 01:37:23 -0400

    fix type

--- zwave_js_server/exceptions.py
+++ zwave_js_server/exceptions.py
@@ -65,9 +65,10 @@ class InvalidServerVersion(BaseZwaveJSServerError):
     """Exception raised when connected to server with incompatible version."""
 
     def __init__(
-        self, version_info: "VersionInfo", required_schema_version: int, message: str
+        self, version_info: Optional["VersionInfo"], required_schema_version: int, message: str
     ) -> None:
         """Initialize an invalid server version error."""
+        assert version_info
         self.server_version = version_info.server_version
         self.server_max_schema_version = version_info.max_schema_version
         self.required_schema_version = required_schema_version
