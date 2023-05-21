fc62500e71dcce2ff4ed3d69721f7a54565d4056
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-05-21 04:22:06 -0400

    Update Window Covering CC constants

--- zwave_js_server/const/command_class/window_covering.py
+++ zwave_js_server/const/command_class/window_covering.py
@@ -1,37 +1,57 @@
 """Constants for the Barrier Operator CC."""
 from __future__ import annotations
 
-from enum import Enum, IntEnum
+from enum import IntEnum
+
+NO_POSITION_SUFFIX = "(no position)"
+WINDOW_COVERING_OPEN_PROPERTY = "open"
+WINDOW_COVERING_CLOSE_PROPERTY = "close"
 
 
-class WindowCoveringPropertyKey(str, Enum):
+class WindowCoveringPropertyKey(IntEnum):
     """Enum of all known Window Covering CC property keys."""
 
     # https://github.com/zwave-js/node-zwave-js/blob/master/packages/cc/src/lib/_Types.ts#L1588
-    HORIZONTAL_SLATS_ANGLE = "Horizontal Slats Angle"
-    HORIZONTAL_SLATS_ANGLE_NO_POSITION = "Horizontal Slats Angle (no position)"
-    INBOUND_BOTTOM = "Inbound Bottom"
-    INBOUND_BOTTOM_NO_POSITION = "Inbound Bottom (no position)"
-    INBOUND_LEFT = "Inbound Left"
-    INBOUND_LEFT_NO_POSITION = "Inbound Left (no position)"
-    INBOUND_LEFT_RIGHT = "Inbound Left/Right"
-    INBOUND_LEFT_RIGHT_NO_POSITION = "Inbound Left/Right (no position)"
-    INBOUND_RIGHT = "Inbound Right"
-    INBOUND_RIGHT_NO_POSITION = "Inbound Right (no position)"
-    INBOUND_TOP = "Inbound Top"
-    INBOUND_TOP_NO_POSITION = "Inbound Top (no position)"
-    INBOUND_TOP_BOTTOM = "Inbound Top/Bottom"
-    INBOUND_TOP_BOTTOM_NO_POSITION = "Inbound Top/Bottom (no position)"
-    OUTBOUND_BOTTOM = "Outbound Bottom"
-    OUTBOUND_BOTTOM_NO_POSITION = "Outbound Bottom (no position)"
-    OUTBOUND_LEFT = "Outbound Left"
-    OUTBOUND_LEFT_NO_POSITION = "Outbound Left (no position)"
-    OUTBOUND_RIGHT = "Outbound Right"
-    OUTBOUND_RIGHT_NO_POSITION = "Outbound Right (no position)"
-    OUTBOUND_TOP = "Outbound Top"
-    OUTBOUND_TOP_NO_POSITION = "Outbound Top (no position)"
-    VERTICAL_SLATS_ANGLE = "Vertical Slats Angle"
-    VERTICAL_SLATS_ANGLE_NO_POSITION = "Vertical Slats Angle (no position)"
+    OUTBOUND_LEFT_NO_POSITION = 0
+    OUTBOUND_LEFT = 1
+    OUTBOUND_RIGHT_NO_POSITION = 2
+    OUTBOUND_RIGHT = 3
+    INBOUND_LEFT_NO_POSITION = 4
+    INBOUND_LEFT = 5
+    INBOUND_RIGHT_NO_POSITION = 6
+    INBOUND_RIGHT = 7
+    INBOUND_LEFT_RIGHT_NO_POSITION = 8
+    INBOUND_LEFT_RIGHT = 9
+    VERTICAL_SLATS_ANGLE_NO_POSITION = 10
+    VERTICAL_SLATS_ANGLE = 11
+    OUTBOUND_BOTTOM_NO_POSITION = 12
+    OUTBOUND_BOTTOM = 13
+    OUTBOUND_TOP_NO_POSITION = 14
+    OUTBOUND_TOP = 15
+    INBOUND_BOTTOM_NO_POSITION = 16
+    INBOUND_BOTTOM = 17
+    INBOUND_TOP_NO_POSITION = 18
+    INBOUND_TOP = 19
+    INBOUND_TOP_BOTTOM_NO_POSITION = 20
+    INBOUND_TOP_BOTTOM = 21
+    HORIZONTAL_SLATS_ANGLE_NO_POSITION = 22
+    HORIZONTAL_SLATS_ANGLE = 23
+
+
+NO_POSITION_PROPERTY_KEYS = {
+    WindowCoveringPropertyKey.OUTBOUND_LEFT_NO_POSITION,
+    WindowCoveringPropertyKey.OUTBOUND_RIGHT_NO_POSITION,
+    WindowCoveringPropertyKey.INBOUND_LEFT_NO_POSITION,
+    WindowCoveringPropertyKey.INBOUND_RIGHT_NO_POSITION,
+    WindowCoveringPropertyKey.INBOUND_LEFT_RIGHT_NO_POSITION,
+    WindowCoveringPropertyKey.VERTICAL_SLATS_ANGLE_NO_POSITION,
+    WindowCoveringPropertyKey.OUTBOUND_BOTTOM_NO_POSITION,
+    WindowCoveringPropertyKey.OUTBOUND_TOP_NO_POSITION,
+    WindowCoveringPropertyKey.INBOUND_BOTTOM_NO_POSITION,
+    WindowCoveringPropertyKey.INBOUND_TOP_NO_POSITION,
+    WindowCoveringPropertyKey.INBOUND_TOP_BOTTOM_NO_POSITION,
+    WindowCoveringPropertyKey.HORIZONTAL_SLATS_ANGLE_NO_POSITION,
+}
 
 
 class SlatStates(IntEnum):
@@ -40,6 +60,3 @@ class SlatStates(IntEnum):
     CLOSED_1 = 0
     OPEN = 50
     CLOSED_2 = 99
-
-
-NO_POSITION_SUFFIX = "(no position)"
