56edea0871bb1c30ddfaa03b049439d676e4a630
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-06-16 00:26:10 -0700

    Bump zwave-js-server-python to 0.40.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "0.49.0"
+VERSION = "0.50.0"
 
 
 setup(
