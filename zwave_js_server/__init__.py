82f4ce4e51f73c10106a207623956c8796ff43ff
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-07-23 18:59:41 -0400

    Remove backport since we are dropping python 3.10 support

--- zwave_js_server/__init__.py
+++ zwave_js_server/__init__.py
@@ -1,9 +1,1 @@
 """Provide a package for zwave-js-server."""
-
-import sys
-
-# monkeypatch fromisoformat for Python 3.10 to support more formats
-if sys.version_info < (3, 11):
-    from backports.datetime_fromisoformat import MonkeyPatch
-
-    MonkeyPatch.patch_fromisoformat()
