099eff643f1f6c54ac79ddfb5985475295780030
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-10-25 09:21:16 -0400

    Bump lib to 0.53.0 (#797)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "0.52.1"
+VERSION = "0.53.0"
 
 
 setup(
