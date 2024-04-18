c9f202d1278a6e7e6601aab5d817515e76c07283
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-04-18 02:25:27 -0400

    Fix tests

--- test/test_client.py
+++ test/test_client.py
@@ -461,7 +461,7 @@ async def test_additional_user_agent_components(client_session, url):
             {
                 "command": "initialize",
                 "messageId": "initialize",
-                "schemaVersion": 34,
+                "schemaVersion": 35,
                 "additionalUserAgentComponents": {
                     "zwave-js-server-python": __version__,
                     "foo": "bar",
