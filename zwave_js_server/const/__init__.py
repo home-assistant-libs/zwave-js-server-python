426ad7da48deafda764efc63792cb5b904bc9687
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-06-20 17:04:10 -0400

    Bump lib to 0.57.0 (#997)

--- zwave_js_server/const/__init__.py
+++ zwave_js_server/const/__init__.py
@@ -8,7 +8,7 @@ import logging
 from typing import TypedDict
 
 PACKAGE_NAME = "zwave-js-server-python"
-__version__ = "0.56.0"
+__version__ = "0.57.0"
 
 # minimal server schema version we can handle
 MIN_SERVER_SCHEMA_VERSION = 36
