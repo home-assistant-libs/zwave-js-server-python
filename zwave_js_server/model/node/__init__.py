1b8f450b7699481ffbfd819786a26fe11f865450
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-07-17 06:21:06 +0000

    fix lint

--- zwave_js_server/model/node/__init__.py
+++ zwave_js_server/model/node/__init__.py
@@ -503,9 +503,7 @@ class Node(EventBase):
         if wait_for_result:
             result = await self.client.async_send_command(message, **kwargs)
             return result
-        if wait_for_result is None and self.status not in (
-            NodeStatus.ASLEEP,
-        ):
+        if wait_for_result is None and self.status not in (NodeStatus.ASLEEP,):
             result_task = asyncio.create_task(
                 self.client.async_send_command(message, **kwargs)
             )
