e9a4ea62826fd1a7e81cbae9ccef8805f5c68a75
Martin Hjelmare <marhje52@gmail.com>
2025-07-24 18:00:17 +0200

    Bump min and max schema version to 44

--- test/test_dump.py
+++ test/test_dump.py
@@ -106,7 +106,7 @@ async def test_dump_additional_user_agent_components(
         {
             "command": "initialize",
             "messageId": "initialize",
-            "schemaVersion": 43,
+            "schemaVersion": 44,
             "additionalUserAgentComponents": {
                 "zwave-js-server-python": __version__,
                 "foo": "bar",
